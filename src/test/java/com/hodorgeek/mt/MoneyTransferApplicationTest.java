9e444e84092c7ff11bd8c150b881680be3824eda
hodorgeek <shambhand2020@gmail.com>
2019-08-04 19:06:21 +0530

    integred junit5 and jacoco and add sample test

---
+++ src/test/java/com/hodorgeek/mt/MoneyTransferApplicationTest.java
@@ -0,0 +1,13 @@
+package com.hodorgeek.mt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MoneyTransferApplicationTest {
+
+    @Test
+    void testSomething() {
+        assertEquals(true, true);
+    }
+}
